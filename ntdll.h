ead89043bad4c1c4368dac38f7a9433ed70c8a9c
winsoft666 <smart7178@163.com>
2025-12-23 14:06:41 +0800

    Update header.

--- ntdll.h
+++ ntdll.h
@@ -1,11 +1,7 @@
 #pragma once
 
 #ifndef _WIN32_WINNT
-#ifdef _WIN64
-#define _WIN32_WINNT 0x0502 // XP x64 is version 5.2
-#else
-#define _WIN32_WINNT 0x0501
-#endif
+#define _WIN32_WINNT 0x0601 // XP x64 is version 5.2, Vista is version 6.0, Windows 7 is version 6.1
 #endif // _WIN32_WINNT
 
 #ifdef WINVER // Overwrite WINVER if given on command line
@@ -14,7 +10,7 @@
 #define WINVER _WIN32_WINNT
 
 #ifndef _WIN32_IE
-#define _WIN32_IE 0x0500
+#define _WIN32_IE _WIN32_IE_WIN7
 #endif //_WIN32_IE
 
 #ifdef __cplusplus
@@ -3925,7 +3921,9 @@ typedef struct _TEB
 
     union
     {
+#if (_WIN32_WINNT >= 0x0601)
         PROCESSOR_NUMBER CurrentIdealProcessor;
+#endif
         ULONG IdealProcessorValue;
         struct
         {
@@ -9064,6 +9062,8 @@ RtlWalkHeap(
     _Inout_ PRTL_HEAP_WALK_ENTRY Entry
 );
 
+// NOTE: mingw had an incorrect definition for these
+#ifndef __MINGW32__
 NTSYSAPI
 NTSTATUS
 NTAPI
@@ -9084,6 +9084,7 @@ RtlSetHeapInformation(
     _In_opt_ PVOID HeapInformation,
     _In_opt_ SIZE_T HeapInformationLength
 );
+#endif // __MINGW__
 
 NTSYSAPI
 SIZE_T
